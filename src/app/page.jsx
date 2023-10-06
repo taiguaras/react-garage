feeef36346b5523f2c20c1c54ff92af7c76c5f19
taiguaras <taiguaras@gmail.com>
2023-10-06 00:45:29 -0300

    Fix main bugs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,10 @@ import Image from 'next/image';
 import Button from '../components/Button';
 import Box from '../components/Box';
 import { Inter } from 'next/font/google'
-import styles from './page.module.css';
+import 'tailwindcss/tailwind.css'
+//import styles from './page.module.css';
+import styles from './globals.css';
+
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +15,9 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <div className="layout">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
           <Button seliga={"Teste"}>Comeon</Button>
           <Box qualquer={"inter"}>Opa</Box>
         </div>
